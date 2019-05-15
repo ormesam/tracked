5b04429956fcc67dae6b7b7e3634bfeac164d0d2
Sam Orme <ormesam@hotmail.com>
2019-05-15 18:20:27 +0100

    Add AccelerometerUtility to handle the accelerometer which we can hopefully use for detecting jumps.

---
+++ MtbMate/MtbMate/MtbMate/Utilities/AccelerometerUtility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Essentials;
+
+namespace MtbMate.Utilities
+{
+    public class AccelerometerUtility
+    {
+        private SensorSpeed speed = SensorSpeed.Default;
+
+        public AccelerometerUtility()
+        {
+            Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+        }
+
+        private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            AccelerometerData data = e.Reading;
+
+            Console.WriteLine($"Reading: X: {data.Acceleration.X}, Y: {data.Acceleration.Y}, Z: {data.Acceleration.Z}");
+        }
+
+        public void Start()
+        {
+            if (!Accelerometer.IsMonitoring)
+            {
+                Accelerometer.Start(speed);
+            }
+        }
+
+        public void Stop()
+        {
+            if (Accelerometer.IsMonitoring)
+            {
+                Accelerometer.Stop();
+            }
+        }
+
+        public void ChangeSpeed(SensorSpeed speed)
+        {
+            Stop();
+            this.speed = speed;
+            Start();
+        }
+    }
+}
