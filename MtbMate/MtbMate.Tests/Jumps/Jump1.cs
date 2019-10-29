ac4e6e9b5e1432a001716b9e97583cbedf52828c
Sam Orme <ormesam@hotmail.com>
2019-10-29 19:56:31 +0000

    Remove tests which had duplicated result sets

--- MtbMate/MtbMate.Tests/Jumps/Jump1.cs
+++ MtbMate/MtbMate.Tests/Jumps/Jump1.cs
@@ -9,11 +9,11 @@ namespace MtbMate.Tests.Jumps {
         public void Analyse() {
             Assert.IsTrue(JumpDetectionUtility.Jumps.Count == 2);
 
-            Assert.AreEqual(0.536, JumpDetectionUtility.Jumps[0].Airtime, 0.0001);
-            Assert.AreEqual(6.63, JumpDetectionUtility.Jumps[0].LandingGForce, 0.1);
+            Assert.AreEqual(0.683, JumpDetectionUtility.Jumps[0].Airtime, 0.0001);
+            Assert.AreEqual(1.2, JumpDetectionUtility.Jumps[0].LandingGForce, 0.1);
 
-            Assert.AreEqual(0.587, JumpDetectionUtility.Jumps[1].Airtime, 0.0001);
-            Assert.AreEqual(1.36, JumpDetectionUtility.Jumps[1].LandingGForce, 0.1);
+            Assert.AreEqual(0.683, JumpDetectionUtility.Jumps[1].Airtime, 0.0001);
+            Assert.AreEqual(4.765, JumpDetectionUtility.Jumps[1].LandingGForce, 0.1);
         }
     }
 }
