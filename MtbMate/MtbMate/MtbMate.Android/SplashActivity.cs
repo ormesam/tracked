b54464c786e4b91f0c0a7a8de501d35a3bb59498
Sam Orme <ormesam@hotmail.com>
2019-11-26 20:07:06 +0000

    Add splashscreen and move icon

--- MtbMate/MtbMate/MtbMate.Android/SplashActivity.cs
+++ MtbMate/MtbMate/MtbMate.Android/SplashActivity.cs
@@ -2,7 +2,7 @@
 using Android.OS;
 
 namespace MtbMate.Droid {
-    [Activity(Theme = "@style/MainTheme.Splash", Icon = "@drawable/ic_launcher", MainLauncher = true, Label = "Mtb Mate", NoHistory = true)]
+    [Activity(Theme = "@style/MainTheme.Splash", Icon = "@mipmap/ic_launcher", MainLauncher = true, Label = "Mtb Mate", NoHistory = true)]
     public class SplashActivity : Activity {
         protected override void OnCreate(Bundle bundle) {
             base.OnCreate(bundle);
