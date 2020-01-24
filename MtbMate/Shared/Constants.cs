91c49783d1279308c175c802a3917fe923301e79
Sam Orme <ormesam@hotmail.com>
2020-01-24 18:20:57 +0000

    Remove the shared project pre & post bild commands

--- MtbMate/Shared/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Shared {
-    public class Constants {
-        public const string GoogleMapsApiKey = "";
-        public const string GoogleAuthRedirectUrl = "";
-        public const string GoogleOAuthApiKey = "";
-        public const string Url = "";
-    }
-}
