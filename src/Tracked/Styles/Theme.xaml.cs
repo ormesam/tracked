db3f3b881ebb5a0d28845f5876e8dfc232d30dd8
Sam Orme <ormesam@hotmail.com>
2021-01-11 18:05:23 +0000

    Add theme resource dictionary

---
+++ src/Tracked/Styles/Theme.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tracked.Styles {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Theme : ResourceDictionary {
+        public Theme() {
+            InitializeComponent();
+        }
+    }
+}
