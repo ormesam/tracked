ce692fabc31032d41e7c9fe13060e265bddc801f
Sam Orme <ormesam@hotmail.com>
2021-08-07 20:23:20 +0100

    Configure speed and add altitude chart

--- src/Tracked/Screens/Rides/SpeedAnalysisScreenViewModel.cs
+++ src/Tracked/Screens/Rides/SpeedAnalysisScreenViewModel.cs
@@ -8,20 +8,39 @@ namespace Tracked.Screens.Rides {
     public class SpeedAnalysisScreenViewModel : ViewModelBase {
         private readonly IList<RideLocationDto> rideLocations;
 
-        public Chart Chart { get; }
+        public Chart SpeedChart { get; }
+        public Chart AltitudeChart { get; }
 
         public SpeedAnalysisScreenViewModel(MainContext context, IList<RideLocationDto> rideLocations) : base(context) {
             this.rideLocations = rideLocations;
 
-            Chart = CreateAnaysisChartModel();
+            SpeedChart = CreateSpeedChart();
+            AltitudeChart = CreateAltitudeChart();
         }
 
         public override string Title => "Speed & Altitude";
 
-        private Chart CreateAnaysisChartModel() {
+        private Chart CreateSpeedChart() {
             return new LineChart {
                 Entries = rideLocations
                     .Select(i => new ChartEntry((float)i.Mph)),
+                LineMode = LineMode.Straight,
+                LineSize = 8,
+                PointMode = PointMode.None,
+                PointSize = 0,
+                MinValue = 0,
+            };
+        }
+
+        private Chart CreateAltitudeChart() {
+            return new LineChart {
+                Entries = rideLocations
+                    .Select(i => new ChartEntry((float)i.Altitude)),
+                LineMode = LineMode.Straight,
+                LineSize = 8,
+                PointMode = PointMode.None,
+                PointSize = 0,
+                MinValue = (float)rideLocations.Min(i => i.Altitude),
             };
         }
     }
